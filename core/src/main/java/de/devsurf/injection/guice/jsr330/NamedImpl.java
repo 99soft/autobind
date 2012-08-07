70a85a7819e409f0d6711b836f798da045a643ef
simonetripodi <simone.tripodi@gmail.com>
2012-08-07 22:08:58 +0200

    code format added license

--- core/src/main/java/de/devsurf/injection/guice/jsr330/NamedImpl.java
+++ core/src/main/java/de/devsurf/injection/guice/jsr330/NamedImpl.java
@@ -1,43 +1,71 @@
 package de.devsurf.injection.guice.jsr330;
 
+/*
+ *    Copyright 2012 The 99 Software Foundation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+import static java.lang.String.format;
+
 import java.io.Serializable;
 import java.lang.annotation.Annotation;
 
 import javax.inject.Named;
 
-class NamedImpl implements Named, Serializable {
+class NamedImpl
+    implements Named, Serializable
+{
 
-	private final String value;
+    private static final long serialVersionUID = 0;
 
-	public NamedImpl(String value) {
-		this.value = value;
-	}
+    private final String value;
 
-	public String value() {
-		return this.value;
-	}
+    public NamedImpl( String value )
+    {
+        this.value = value;
+    }
 
-	public int hashCode() {
-		// This is specified in java.lang.Annotation.
-		return (127 * "value".hashCode()) ^ value.hashCode();
-	}
+    public String value()
+    {
+        return this.value;
+    }
 
-	public boolean equals(Object o) {
-		if (!(o instanceof Named)) {
-			return false;
-		}
+    public int hashCode()
+    {
+        // This is specified in java.lang.Annotation.
+        return ( 127 * "value".hashCode() ) ^ value.hashCode();
+    }
 
-		Named other = (Named) o;
-		return value.equals(other.value());
-	}
+    public boolean equals( Object o )
+    {
+        if ( !( o instanceof Named ) )
+        {
+            return false;
+        }
 
-	public String toString() {
-		return "@" + Named.class.getName() + "(value=" + value + ")";
-	}
+        Named other = (Named) o;
+        return value.equals( other.value() );
+    }
 
-	public Class<? extends Annotation> annotationType() {
-		return Named.class;
-	}
+    public String toString()
+    {
+        return format( "@%s(value=%s)", Named.class.getName(), value );
+    }
 
-	private static final long serialVersionUID = 0;
+    public Class<? extends Annotation> annotationType()
+    {
+        return Named.class;
+    }
+
 }
--- core/src/main/java/de/devsurf/injection/guice/jsr330/Names.java
+++ core/src/main/java/de/devsurf/injection/guice/jsr330/Names.java
@@ -1,15 +1,40 @@
 package de.devsurf.injection.guice.jsr330;
 
+/*
+ *    Copyright 2012 The 99 Software Foundation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
 import javax.inject.Named;
 
-public class Names {
+public class Names
+{
 
-	  private Names() {}
+    /**
+     * Private constructor, this class must not be instantiated.
+     */
+    private Names()
+    {
+        // does nothing
+    }
 
-	  /**
-	   * Creates a {@link Named} annotation with {@code name} as the value.
-	   */
-	  public static Named named(String name) {
-	    return new NamedImpl(name);
-	  }
+    /**
+     * Creates a {@link Named} annotation with {@code name} as the value.
+     */
+    public static Named named( String name )
+    {
+        return new NamedImpl( name );
+    }
+
 }
