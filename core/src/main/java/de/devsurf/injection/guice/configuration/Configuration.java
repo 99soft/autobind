71f3ea0b9d4e2edc2a3a00446090b88ea5cbe72b
simonetripodi <simone.tripodi@gmail.com>
2012-08-08 08:58:23 +0200

    headers code format static imports q: can this class moved to the annotations module?

--- core/src/main/java/de/devsurf/injection/guice/configuration/Configuration.java
+++ core/src/main/java/de/devsurf/injection/guice/configuration/Configuration.java
@@ -1,23 +1,27 @@
-/**
- * Copyright (C) 2010 Daniel Manzke <daniel.manzke@googlemail.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package de.devsurf.injection.guice.configuration;
 
-import java.lang.annotation.ElementType;
+/*
+ *    Copyright 2012 The 99 Software Foundation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+import static de.devsurf.injection.guice.configuration.Configuration.Type.CONFIGURATION;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.util.Properties;
 
@@ -26,72 +30,71 @@ import javax.inject.Named;
 /**
  * Use this Annotation to express your need, that a Configuration should be
  * loaded, so it can be bound to an Object.
- * 
+ *
  * @author Daniel Manzke
- * 
  */
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.TYPE)
-public @interface Configuration {
-	/**
-	 * Name the Configuration should be bound to.
-	 * 
-	 * @return Name the Configuration should be bound to.
-	 */
-	Named name() default @Named("");
+@Retention( RUNTIME )
+@Target( TYPE )
+public @interface Configuration
+{
 
-	/**
-	 * Path/URL where the Configuration could be found.
-	 * 
-	 * @return Path/URL where the Configuration could be found.
-	 */
-	PathConfig location();
+    /**
+     * Name the Configuration should be bound to.
+     *
+     * @return Name the Configuration should be bound to.
+     */
+    Named name() default @Named( "" );
 
-	PathConfig alternative() default @PathConfig("");
+    /**
+     * Path/URL where the Configuration could be found.
+     *
+     * @return Path/URL where the Configuration could be found.
+     */
+    PathConfig location();
 
-	/**
-	 * Class/Interface where the Configuration should be bound to.
-	 * 
-	 * @return Class/Interface where the Configuration should be bound to.
-	 */
-	Class<? extends Object> to() default Properties.class;
+    PathConfig alternative() default @PathConfig( "" );
 
-	/**
-	 * This does only make sense if you are using the Provider Interface.
-	 * 
-	 * public class Service{
-	 * 
-	 * @Inject Provider<Properties> properties;
-	 * 
-	 *         public void do(){ Properties properties = properties.get(); } }
-	 * 
-	 * @return true if the Configuration should be load lazy
-	 */
-	boolean lazy() default false;
+    /**
+     * Class/Interface where the Configuration should be bound to.
+     *
+     * @return Class/Interface where the Configuration should be bound to.
+     */
+    Class<? extends Object> to() default Properties.class;
 
-	/**
-	 * Specify what should be bound for the Configuration
-	 * 
-	 * Configuration -> Configuration only Values -> Named Bindings for the
-	 * Values Both -> Configuration and Named Bindings
-	 * 
-	 * @return
-	 */
-	Type type() default Type.CONFIGURATION;
+    /**
+     * This does only make sense if you are using the Provider Interface. public class Service{
+     *
+     * @Inject Provider<Properties> properties; public void do(){ Properties properties = properties.get(); } }
+     * @return true if the Configuration should be load lazy
+     */
+    boolean lazy() default false;
 
-	public enum Type {
-		/**
-		 * Binds only the Configuration itself.
-		 */
-		CONFIGURATION,
-		/**
-		 * Named Bindings for the Key/Values (only possible if not lazy)
-		 */
-		VALUES,
-		/**
-		 * The Configurationd and Named Bindings will be done. (only possible if
-		 * not lazy)
-		 */
-		BOTH
-	}
+    /**
+     * Specify what should be bound for the Configuration Configuration -> Configuration only Values -> Named Bindings
+     * for the Values Both -> Configuration and Named Bindings
+     *
+     * @return
+     */
+    Type type() default CONFIGURATION;
+
+    public enum Type
+    {
+
+        /**
+         * Binds only the Configuration itself.
+         */
+        CONFIGURATION,
+
+        /**
+         * Named Bindings for the Key/Values (only possible if not lazy)
+         */
+        VALUES,
+
+        /**
+         * The Configuration and Named Bindings will be done. (only possible if not lazy)
+         */
+        BOTH
+
+    }
+
 }
