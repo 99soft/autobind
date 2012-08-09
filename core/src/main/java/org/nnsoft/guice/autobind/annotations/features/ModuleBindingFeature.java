e19188b12338173a1fbe5648a312e689d501229a
simonetripodi <simone.tripodi@gmail.com>
2012-08-09 14:10:57 +0200

    relicensed, code format

--- core/src/main/java/org/nnsoft/guice/autobind/annotations/features/ModuleBindingFeature.java
+++ core/src/main/java/org/nnsoft/guice/autobind/annotations/features/ModuleBindingFeature.java
@@ -1,7 +1,20 @@
-/**
- * 
- */
 package org.nnsoft.guice.autobind.annotations.features;
+
+/*
+ *    Copyright 2012 The 99 Software Foundation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 import java.lang.annotation.Annotation;
 import java.util.Map;
@@ -18,39 +31,53 @@ import org.nnsoft.guice.autobind.scanner.features.BindingScannerFeature;
 
 import com.google.inject.Module;
 
-
 @Singleton
-public class ModuleBindingFeature extends BindingScannerFeature {
-	private Logger _logger = Logger.getLogger(ModuleBindingFeature.class.getName());
+public class ModuleBindingFeature
+    extends BindingScannerFeature
+{
 
-	@Override
-	public BindingStage accept(Class<Object> annotatedClass, Map<String, Annotation> annotations) {
-		if (annotations.containsKey(GuiceModule.class.getName())) {
-			GuiceModule module = (GuiceModule) annotations.get(GuiceModule.class.getName());
-			return module.stage();
-		}
-		return BindingStage.IGNORE;
-	}
+    private Logger _logger = Logger.getLogger( ModuleBindingFeature.class.getName() );
 
-	@Override
-	public void process(final Class<Object> annotatedClass, Map<String, Annotation> annotations) {
-		BindingJob job = new ModuleBindingJob(annotatedClass.getName());
-		if(!tracer.contains(job)){
-			if (_logger.isLoggable(Level.INFO)) {
-				_logger.info("Installing Module: " + annotatedClass.getName());
-			}
-			synchronized (_binder) {
-				_binder.install((Module) injector.getInstance(annotatedClass));
-			}			
-		}else {
-			if (_logger.isLoggable(Level.FINE)) {
-				_logger.log(Level.FINE, "Ignoring BindingJob \"" + job.toString()
-						+ "\", because it was already bound.", new Exception("Ignoring BindingJob \"" + job.toString()
-							+ "\", because it was already bound."));
-			} else if (_logger.isLoggable(Level.INFO)) {
-				_logger.log(Level.INFO, "Ignoring BindingJob \"" + job.toString()
-						+ "\", because it was already bound.");
-			}
-		}
-	}
+    @Override
+    public BindingStage accept( Class<Object> annotatedClass, Map<String, Annotation> annotations )
+    {
+        if ( annotations.containsKey( GuiceModule.class.getName() ) )
+        {
+            GuiceModule module = (GuiceModule) annotations.get( GuiceModule.class.getName() );
+            return module.stage();
+        }
+        return BindingStage.IGNORE;
+    }
+
+    @Override
+    public void process( final Class<Object> annotatedClass, Map<String, Annotation> annotations )
+    {
+        BindingJob job = new ModuleBindingJob( annotatedClass.getName() );
+        if ( !tracer.contains( job ) )
+        {
+            if ( _logger.isLoggable( Level.INFO ) )
+            {
+                _logger.info( "Installing Module: " + annotatedClass.getName() );
+            }
+            synchronized ( _binder )
+            {
+                _binder.install( (Module) injector.getInstance( annotatedClass ) );
+            }
+        }
+        else
+        {
+            if ( _logger.isLoggable( Level.FINE ) )
+            {
+                _logger.log( Level.FINE, "Ignoring BindingJob \"" + job.toString()
+                    + "\", because it was already bound.", new Exception( "Ignoring BindingJob \"" + job.toString()
+                    + "\", because it was already bound." ) );
+            }
+            else if ( _logger.isLoggable( Level.INFO ) )
+            {
+                _logger.log( Level.INFO, "Ignoring BindingJob \"" + job.toString()
+                    + "\", because it was already bound." );
+            }
+        }
+    }
+
 }
