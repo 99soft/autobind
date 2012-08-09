391a1d8170565be417772df7123de2ab7bfa35cd
simonetripodi <simone.tripodi@gmail.com>
2012-08-09 22:22:56 +0200

    relicensed sources

--- aop/src/main/java/org/nnsoft/guice/autobind/aop/MethodMatcher.java
+++ aop/src/main/java/org/nnsoft/guice/autobind/aop/MethodMatcher.java
@@ -1,22 +1,20 @@
-/**
- * Copyright (C) 2010 Daniel Manzke <daniel.manzke@googlemail.com>
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * 
- */
 package org.nnsoft.guice.autobind.aop;
+
+/*
+ *    Copyright 2012 The 99 Software Foundation
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -30,12 +28,12 @@ import org.aopalliance.intercept.MethodInterceptor;
  * This Annotation marks a Method, which returns an Object of Type
  * {@link Matcher}. This Matcher is used by Guice, to decide if a
  * {@link MethodInterceptor} should be invoked for that {@link Method}.
- * 
+ *
  * @MethodMatcher public Matcher<? super Method> getMethodMatcher() { return
  *                Matchers.annotatedWith(Intercept.class); }
- * 
+ *
  * @author Daniel Manzke
- * 
+ *
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target( { ElementType.METHOD })
