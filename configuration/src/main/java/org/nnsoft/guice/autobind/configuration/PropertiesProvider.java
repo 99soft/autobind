64b21f72da02e54021719acded9ec639ba0dc82c
simonetripodi <simone.tripodi@gmail.com>
2012-08-10 10:37:39 +0200

    logger can be final

--- configuration/src/main/java/org/nnsoft/guice/autobind/configuration/PropertiesProvider.java
+++ configuration/src/main/java/org/nnsoft/guice/autobind/configuration/PropertiesProvider.java
@@ -27,7 +27,7 @@ public class PropertiesProvider
     implements Provider<Properties>
 {
 
-    Logger _logger = Logger.getLogger( PropertiesProvider.class.getName() );
+    private final Logger _logger = Logger.getLogger( PropertiesProvider.class.getName() );
 
     private URL url;
 
